27b1f58a5fc00197caa3994d277f58bad81c6309
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2025-11-25 11:54:37 +0900

    Update phantom to v2.2.4 (#8)

--- Formula/phantom.rb
+++ Formula/phantom.rb
@@ -1,8 +1,8 @@
 class Phantom < Formula
   desc "Git worktree manager with enhanced functionality"
   homepage "https://github.com/aku11i/phantom"
-  url "https://registry.npmjs.org/@aku11i/phantom/-/phantom-2.2.3.tgz"
-  sha256 "6519a79cb7e55a23ec5c97db57d0f0da6f4b2f90fd8289d6f2e9d3ddbb8ef673"
+  url "https://registry.npmjs.org/@aku11i/phantom/-/phantom-2.2.4.tgz"
+  sha256 "a9a4162bd6793a2ae7c68865eb1f524d25f5d8feef3bdf1886c37a86018ca179"
   license "MIT"
 
   depends_on "node"
